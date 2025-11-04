2fb4c55def17822f575921247a8e2f97f0232f93
Aditya Yadav <adityayadav1192006@gamil.com>
2025-11-04 20:54:54 +0530

    Initial commit: Base OS project setup

---
+++ main.py
@@ -0,0 +1,7 @@
+from app import app
+
+# Entry point of the application
+if __name__ == "__main__":
+    # Run the application
+    # Ensure debug mode is only enabled in development
+    app.run(host="0.0.0.0", port=4000, debug=True)  # Set xdebug=False in production
